ad842698c3cd5319be768d971ebd217b786d83ec
Is-haka <isihakamabo5@gmail.com>
2021-02-20 01:37:55 +0300

    another modification on database connectivity

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,5 +1,5 @@
 <?php 
 
 // $conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
-$conn= new mysqli('remotemysql.com','xExF139JDU','JrSHujxdyi','xExF139JDU')or die("Could not connect to mysql".mysqli_error($con));
+$conn= mysqli_connect('remotemysql.com','xExF139JDU','JrSHujxdyi','xExF139JDU')or die("Could not connect to mysql".mysqli_error($con));
 
