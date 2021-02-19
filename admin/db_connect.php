11fac1d24b9c53df71bd7cd72212634d4b0fa3b3
Is-haka <isihakamabo5@gmail.com>
2021-02-20 00:37:08 +0300

    new databases credetial file changed

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,5 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
+// $conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('remotemysql.com','xExF139JDU','JrSHujxdyi','xExF139JDU')or die("Could not connect to mysql".mysqli_error($con));
+
