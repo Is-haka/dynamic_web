3bab1b0d62420495280af79dcfecbc43944469f7
Is-haka <isihakamabo5@gmail.com>
2021-02-22 15:55:54 +0300

    changed database

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,5 +1,5 @@
 <?php 
 
 // $conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
-$conn= mysqli_connect('remotemysql.com','xExF139JDU','JrSHujxdyi','xExF139JDU')or die("Could not connect to mysql".mysqli_error($con));
+$conn= mysqli_connect('remotemysql.com','VgPm3cm6Tx','uFNgF7iHJS','VgPm3cm6Tx')or die("Could not connect to mysql".mysqli_error($con));
 
